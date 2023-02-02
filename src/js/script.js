6764711244628eb97577f50b87c7d2034a83f0b8
Vladislav Kohai <cherry.designwork@gmail.com>
2023-02-02 12:48:18 +0300

    Adding slider step 1

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    const slider = tns({
+        container: '.feedback__slider',
+        items: 4,
+        slideBy: 'page',
+        controls: false,
+        nav: false
+    });
+});
